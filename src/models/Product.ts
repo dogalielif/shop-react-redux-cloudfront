812ee591010aeb2748ae2412b2fa59f8d58a6708
Elif Dogali <dogali.elif@gmail.com>
2022-07-24 18:48:13 +0300

    Fix product type

--- src/models/Product.ts
+++ src/models/Product.ts
@@ -5,10 +5,13 @@ export type Product = {
   title: string,
   description: string,
   price: number,
+  count: number
 };
 
 export const ProductSchema = Yup.object().shape({
+  id: Yup.string().required(),
   title: Yup.string().required(),
   description: Yup.string(),
   price: Yup.number().required(),
+  count: Yup.number().required()
 });
