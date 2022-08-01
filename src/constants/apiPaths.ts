1f2d5183b9e012c1d7f8c0e2ae00c39f0274142f
Elif Dogali <dogali.elif@gmail.com>
2022-08-01 13:55:31 +0300

    Fix api path for import

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@
 const API_PATHS = {
   product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://9x9v5omx25.execute-api.us-east-1.amazonaws.com/dev/import',
+  import: 'https://9x9v5omx25.execute-api.us-east-1.amazonaws.com/dev',
   bff: 'https://n9ubaish76.execute-api.eu-west-1.amazonaws.com/dev',
   cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 };
